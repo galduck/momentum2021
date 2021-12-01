403f483150c8d96722741090beac889862d92112
Miae Je <92987632+galduck@users.noreply.github.com>
2021-12-01 23:30:52 +0900

    삭제 기능 구현
     삭제 기능 구현 (Deleting To Dos)
      - 삭제 기능 구현
      - 원하는 To Do 항목을 삭제할 수 있는 기능 구현

--- js/todo.js
+++ js/todo.js
@@ -2,8 +2,18 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-function deleteToDo(){
-  console.log("lalalal");
+const TODOS_KEY = "todos";
+
+let toDos =[];
+
+function saveToDos(){ 
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); 
+  // 이 function의 역할은 toDos array를 localStorage에 집어 넣는 것 
+}
+
+function deleteToDo(event){
+  const li = event.target.parentElement; // 원하는 항목 하나를 부모랑 같이 선택
+  li.remove(); // 원하는 항목 하나만 삭제  
 }
 
 function paintToDo(newTodo){ // ToDo를 그리는 역할
@@ -27,8 +37,21 @@ function handleToDoSubmit(event){
   event.preventDefault(); // input을 submit 해도 새로고침이 일어나지 않음 
   const newTodo = toDoInput.value; // 입력받은 todo 값을 newTodo에 저장하기 
   toDoInput.value = ""; // todo 입력 다 받았으면 인풋칸을 비워주기
-  paintToDo(newTodo);
+  toDos.push(newTodo); // 입력받은 텍스트(newTodo)를 toDos Array에 push하고 
+  paintToDo(newTodo); // 그 다음 화면에 toDo를 그려줌
+  saveToDos(); // 그리고 나서 ToDo들을 저장함 
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 // submit 버튼을 클릭해야 handleToDoSubmit이 실행됨
+
+// function sayHello(item){
+//   console.log("this is the turn of", item);
+// }
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos != null){
+  const parsedToDos = JSON.parse(savedToDos);
+  parsedToDos.forEach(paintToDo);
+}
